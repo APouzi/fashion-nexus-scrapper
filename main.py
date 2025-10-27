d05c24ae7b74387cf363bdc8cf266c01e632194e
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2025-10-26 17:52:37 -0700

    first commit -AP

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+class FashionScraper:
+    logger = None #placeholder for logger
+    sites_to_scrape: list = []
+    
+    def run(self):
+        print("Sites to scrape in run function:",self.sites_to_scrape)
+    
+    
